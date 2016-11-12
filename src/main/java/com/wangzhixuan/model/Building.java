1d91bb311c4ccc39d36d3d383b6f52e05ebc263c
zeisschoice <zesschoice@gmail.com>
2016-11-12 16:09:34 +0800

    update

---
+++ src/main/java/com/wangzhixuan/model/Building.java
@@ -0,0 +1,7 @@
+package com.wangzhixuan.model;
+
+import java.io.Serializable;
+
+public class Building implements Serializable {
+
+}
---
+++ src/main/java/com/wangzhixuan/model/Cost.java
@@ -0,0 +1,7 @@
+package com.wangzhixuan.model;
+
+import java.io.Serializable;
+
+public class Cost implements Serializable {
+
+}
